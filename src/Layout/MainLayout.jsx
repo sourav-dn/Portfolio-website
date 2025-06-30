eb48dbed407c351781e0512c1826497be1c82d7e
sourav-dn <souravdn.dev@gmail.com>
2025-06-30 10:04:50 +0600

    modify home

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 import Footer from '../Pages/Shared/Footer/Footer';
 
--- src/Pages/Skills/Skills.jsx
+++ src/Pages/Skills/Skills.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { motion } from "framer-motion";
 
 // Importing icons from React Icons
